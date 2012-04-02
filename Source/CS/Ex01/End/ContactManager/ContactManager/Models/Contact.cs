4b30e7dff19d3be9bfbc4f8ad04555ba6206aa70
brady gaster <brady.gaster@gmail.com>
2012-04-02 11:30:39 -0700

    initial revision

---
+++ Source/CS/Ex01/End/ContactManager/ContactManager/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Source/CS/Ex03/Begin/ContactManager/ContactManager/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactManager.Parts;
+
+namespace ContactManager.Controllers
+{
+	public class HomeController : Controller
+	{
+        private IContactRepository _contactRepository;
+
+        public HomeController(IContactRepository contactRepo)
+        {
+            _contactRepository = contactRepo;
+        }
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
